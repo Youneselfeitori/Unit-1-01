c1cb244de85a1d051405c3e97ac51849b53787a6
Younus Elfeitori <younus@ip-172-31-8-86.ca-central-1.compute.internal>
2018-09-07 18:42:26 +0000

    hello world program

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+Created by: Younes Elfeitori
+Created on: 7/09/2018
+This is a Hello, World! program 
+*/
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello. World!");
+
+	}
+
+}
